a242d46c4c3bc328e12a6f83d6659798085ce80f
~pranathi~ <96736864+Pranathi001@users.noreply.github.com>
2022-05-07 12:25:52 +0530

    #include<stdio.h> int main() {     int rows,i,j,k;     scanf("%d",&rows);     for(i=rows;i>=1;i--)     {         for(j=1;j<=i-1;j++)         {             printf(" ");         }         for(k=1;k<=rows;k++)         {             if(i==1||i==rows||k==1||k==rows)             printf("*");             else             printf(" ");         }         printf(" ");     }     return 0; }

---
+++ print_the_following_pattern.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int rows,i,j,k;
+    scanf("%d",&rows);
+    for(i=rows;i>=1;i--)
+    {
+        for(j=1;j<=i-1;j++)
+        {
+            printf(" ");
+        }
+        for(k=1;k<=rows;k++)
+        {
+            if(i==1||i==rows||k==1||k==rows)
+            printf("*");
+            else
+            printf(" ");
+        }
+        printf("
+");
+    }
+    return 0;
+}
