37a03c2709ce6b5252cf47ad36e07edd43515cde
~pranathi~ <96736864+Pranathi001@users.noreply.github.com>
2022-06-01 14:50:55 +0530

    #include<stdio.h> #include<math.h> int main() {     int i,n,sum,sum1,dif;     int arr[n];     scanf("%d",&n);     sum=0;     sum1=0;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {        if(arr[i]%2==0)         {             sum=sum+arr[i];         }         else         {         sum1=sum1+arr[i];         }     }     dif=abs(sum1-sum);     printf("%d",dif);     return 0; }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_elements.c
@@ -0,0 +1,30 @@
+
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int i,n,sum,sum1,dif;
+    int arr[n];
+    scanf("%d",&n);
+    sum=0;
+    sum1=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+       if(arr[i]%2==0)
+        {
+            sum=sum+arr[i];
+        }
+        else
+        {
+        sum1=sum1+arr[i];
+        }
+    }
+    dif=abs(sum1-sum);
+    printf("%d",dif);
+    return 0;
+}
