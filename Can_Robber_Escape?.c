2b0edf05bdef66742d692798cd8ac2a8c84a1583
~pranathi~ <96736864+Pranathi001@users.noreply.github.com>
2022-07-13 07:19:42 +0530

    #include<stdio.h> int main() {     int n,i,x[20],add=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&x[i]);     }     for(i=0;i<n;i++)     {         if(x[i]%2!=0)         {             add++;         }     }     if(add<=2)     {         printf("YES");     }     else     {         printf("NO");     }     return 0; }

---
+++ Can_Robber_Escape?.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,x[20],add=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(x[i]%2!=0)
+        {
+            add++;
+        }
+    }
+    if(add<=2)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    return 0;
+}
